6ad2c01c8c0735e45e60238bbd6488202639329f
cyanryann <209932180+cyanryann@users.noreply.github.com>
2025-10-15 00:17:47 -0400

    Worked on book adding feature

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const myLibrary = [];
 const lib = document.querySelector('.library');
+const dialog = document.getElementById('bookadding');
+myForm = document.getElementById("sub");
 function Book(name, author, pages, hasRead)
 {
     this.name = name;
@@ -17,15 +19,36 @@ function display() {
     myLibrary.forEach(element => {
         newBook = document.createElement('div');
         title = document.createElement('p');
-        title.innerHTML = element.name + " by " + element.author
+        title.innerHTML = element.name + " by " + element.author + ". \n Read: " + element.hasRead;
         lib.appendChild(newBook);
         newBook.appendChild(title);
     });
 }
+function clear() {
+    while (lib.firstChild)
+    {
+        lib.removeChild(lib.firstChild);
+    }
+}
+function popUp() {
+    dialog.show();
+}
+function loadBook(event) {
+    const bname = document.getElementById("bname").value;
+    const aname = document.getElementById("aname").value;
+    const pcount = document.getElementById("pcount").value;
+    const hread = document.getElementById("hread").value;
+    console.log(bname);
+    addToLibrary(bname, aname, pcount, hread);
+    clear();
+    display();
+    event.preventDefault();
+}
+myForm.addEventListener("submit", loadBook);
 addToLibrary("House of Leaves", "Mark Z. Danielewski", 561, false);
 addToLibrary("House of Leaves", "Mark Z. Danielewski", 561, false);
 addToLibrary("House of Leaves", "Mark Z. Danielewski", 561, false);
 addToLibrary("House of Leaves", "Mark Z. Danielewski", 561, false);
 addToLibrary("House of Leaves", "Mark Z. Danielewski", 561, false);
 addToLibrary("House of Leaves", "Mark Z. Danielewski", 561, false);
-display()
+display();
